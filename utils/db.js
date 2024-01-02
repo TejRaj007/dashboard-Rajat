57d1b7e31cdea98d651b0a1d63d48e1851de82a2
Rajat_Namdev <rajat.namdev@foregahead.io>
2024-01-02 16:24:49 +0530

    env change

--- utils/db.js
+++ utils/db.js
@@ -22,12 +22,12 @@ async function connect() {
 
 async function disconnect() {
   if (connection.isConnected) {
-    if (process.env.NODE_ENV === 'production') {
-      //await mongoose.disconnect();
-      //connection.isConnected = false;
-    } else {
-      console.log('not disconnected');
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   //await mongoose.disconnect();
+    //   //connection.isConnected = false;
+    // } else {
+    //   console.log('not disconnected');
+    // }
   }
 }
 function convertDocToObj(doc) {
